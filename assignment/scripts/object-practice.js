9b912f5f28912fed64e84bf079a3b3db3bce220c
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-04-23 16:24:42 -0500

    Created a variable called fullName and console log it out.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -11,7 +11,7 @@ console.log('***** Object Practice *****')
 const me = {
   // TODO - add properties here
     firstName: 'Isaac',
-    LastName: 'Faulkner',
+    lastName: 'Faulkner',
     hasSiblings: true,
     shoeCount: 6,
     favThreeFoods: ['Strawberries', 'blueberries', 'kiwi']
@@ -24,7 +24,8 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
+let fullName = `${me.firstName}, ${me.lastName}`;
+console.log(fullName);
 
 
 /* 3. Nested arrays
